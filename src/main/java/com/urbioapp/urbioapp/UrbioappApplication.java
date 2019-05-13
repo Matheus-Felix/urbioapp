2beac7dc9c784900ea38df5fd5d9f0791382b689
Matheus <matheusgfelix@gmail.com>
2019-05-13 16:31:24 -0300

    First commit

---
+++ src/main/java/com/urbioapp/urbioapp/UrbioappApplication.java
@@ -0,0 +1,13 @@
+package com.urbioapp.urbioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbioappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbioappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbioapp/urbioapp/UrbioappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.urbioapp.urbioapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrbioappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
